ca048ff7f998f85a87f4445a9620520cec19c5a6
Gautam Gupta <87703240+codewithgautamgupta@users.noreply.github.com>
2022-01-12 11:50:56 +0530

    Add files via upload
    1st commit

---
+++ bulb.js
@@ -0,0 +1,12 @@
+// turn on function 
+
+function bulbOn() {
+    let Bulbobj = document.querySelector(".bulb-off")
+    Bulbobj.setAttribute("src", "bulb-on.png")
+}
+
+// turn off function 
+function bulbOff() {
+    let Bulbobj = document.querySelector(".bulb-off")
+    Bulbobj.setAttribute("src", "bulb-off.png")
+}
